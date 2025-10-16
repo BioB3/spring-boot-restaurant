e5a82f35da0d8b5237b8eceab65c22ed865e16cb
Phasin Sae-ngow <bird.biob3@gmail.com>
2025-10-16 21:32:54 +0700

    Lab Tutorial

---
+++ src/main/java/th/ac/ku/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,17 @@
+package th.ac.ku.restaurant.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import th.ac.ku.restaurant.entity.Restaurant;
+
+@Repository
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, UUID> {
+
+  Restaurant findByName(String name);
+  List<Restaurant> findByLocation(String location);  
+}
